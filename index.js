4620a62f15ea0f3acd54b671fe808711da73b70a
secretwebsite <60954459+secretwebsite@users.noreply.github.com>
2020-02-16 16:19:05 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+function change()
+{
+    setInterval(function(){
+        var num = Math.ceil(Math.random()*14);
+        console.log(num);
+        $("#loveBox").attr("src","hearts/"+num+".png");
+    }, 100);
+}
+$(document).ready(function(){
+    $("#play").click(function(){
+        $(this).addClass("rotate");
+        setTimeout(function(){
+            $("#play").remove();
+            $("#titleBox").append("<h1 id=\"title\">Happy one month Lindsay!</h1>");
+            setTimeout(function(){
+                $("#titleBox").append("<h1 id=\"line1\">I didn't really know what to put on this website.</h1>");
+                setTimeout(function(){
+                    $("#titleBox").append("<h1 id=\"line2\">But at the end of the day,</h1>");
+                    setTimeout(function(){
+                        $("#titleBox").append("<h1 id=\"line3\">I want you to know...</h1>");
+                        setTimeout(function(){
+                            $("#titleBox").append("<h1 id=\"line4\">I love you!!!</h1>");
+                            $("#song")[0].play();
+                            setTimeout(function(){
+                                $("h1").fadeOut("slow",function(){});
+                                setTimeout(function(){
+                                    $("h1").remove();
+                                    $("#titleBox").append("<img id=\"loveBox\" src=\"\" />");
+                                    change();
+                                },2000);
+                            },2000);
+                        },2000);
+                    },2000);
+                },2000);
+            },2000);
+        }, 1000);
+    });
+});
